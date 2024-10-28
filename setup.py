ee914be933f9d7e27a965498e28fbad0d72023f6
assafyaron <128134935+assafyaron@users.noreply.github.com>
2024-10-28 21:07:51 +0200

    Update setup.py final_v

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import Extension, setup
 
+"""
+setup.py file for SymNMF module
+"""
+
 module = Extension('mysymnmfsp', sources=['symnmfmodule.c', 'symnmf.c'], include_dirs=['./'])
 
 setup(
