fe9c2c9d250c64d33bfc767461f83fd331a9b500
assafyaron <128134935+assafyaron@users.noreply.github.com>
2024-10-28 21:09:31 +0200

    Update symnmf.py final_v

--- symnmf.py
+++ symnmf.py
@@ -6,53 +6,75 @@ import mysymnmfsp as SymNMF
 
 np.random.seed(1234)
 
-def initialize_H(w_mat, N, k):
-    # Calculate the average of all entries in w_mat
-    m = np.mean(w_mat)
+"""
+Initialize the matrix H (decomposition matrix) for Symmetric Non-negative Matrix Factorization (SymNMF).
 
-    # Calculate the upper bound for the random values
-    upper_bound = 2 * np.sqrt(m / k)
-    
-    # Initialize H with random values from the interval [0, upper_bound]
-    H = np.random.uniform(low=0, high=upper_bound, size=(N, k))
-    
-    return H
+This function initializes the matrix H with random values from the interval [0, 2*sqrt(m/k)],
+where m is the mean of the entries in the input matrix w_mat and k is the number of centroids.
+
+Parameters:
+w_mat (list): A list of list of float representing the normalized similarity matrix.
+N (int): The number of vectors (rows in the matrix H).
+k (int): The number of centroids (columns in the matrix H).
+
+Returns:
+list: A list of list of float of shape (N, k) representing a matrix initialized with random values.
+"""
+def initializeH(w_mat, N, k):
+    m = np.mean(w_mat) # Calculate the average of all entries in w_mat
+    upper_bound = 2 * np.sqrt(m / k) # Calculate the upper bound for the random values
+    H = np.random.uniform(low=0, high=upper_bound, size=(N, k)) # Initialize H with random values from the interval [0, upper_bound]
+    return H.tolist()
+
+"""
+Perform Symmetric Non-negative Matrix Factorization (SymNMF) on the given vectors.
+
+This function normalizes the input vectors to calculate the W matrix (normalized similarity matrix),
+initializes the H matrix (decomposition matrix),
+and then performs SymNMF to calculate the resulting matrix.
+
+Parameters:
+vectors (list of list of float): A list of lists representing the input vectors.
+k (int): The number of clusters to form.
+
+Returns:
+list: A list of list of float representing the resulting matrix after performing SymNMF.
+"""
+def doSymnmf(vectors, k):
+    w_mat = SymNMF.norm(vectors) # Calling norm function in C to calculate W matrix
+    h_mat = initializeH(w_mat, len(vectors), k) # Initialize H matrix
+    matrix_goal = SymNMF.symnmf(w_mat, h_mat, k) # Calling symnmf function in C to calculate the matrix
+    return matrix_goal
 
 def main():
     try:
         # Get data from console
         input_data = sys.argv
         k, goal, input_file = int(input_data[1]), input_data[2], input_data[3]
-        iter, eps = 300, 0
 
-        # Create Vectors dataframe from csv and save dimension and number of vectors
+        # Create Vectors dataframe from csv file
         vectors = pd.read_csv(input_file, header=None)
-        N = int(len(vectors))
-        vecdim = len(vectors.columns)
         # Convert vectors to python list of lists
         vectors = vectors.values.tolist()
         
         matrix_goal = None # The matrix to calculate and return
 
         # Choose which matrix to calculate and return
-        match goal:
-            case "sym":
-                matrix_goal = SymNMF.sym(vectors, N, vecdim) # Calling sym function in C to calculate the matrix
-            case "ddg":
-                matrix_goal = SymNMF.ddg(vectors, N, vecdim) # Calling ddg function in C to calculate the matrix
-            case "norm":
-                matrix_goal = SymNMF.norm(vectors, N, vecdim) # Calling norm function in C to calculate the matrix
-            case "symnmf":
-                w_mat = SymNMF.norm(vectors, N, vecdim) # Calling norm function in C to calculate W matrix
-                h_mat = initialize_H(w_mat, N, k) # Initialize H matrix
-                matrix_goal = SymNMF.symnmf(w_mat, h_mat, N, vecdim, k, iter, eps) # Calling symnmf function in C to calculate the matrix
-            case _:
-                print("An Error Has Occurred")
-                return
+        if goal == "sym":
+            matrix_goal = SymNMF.sym(vectors) # Calling sym function in C to calculate the matrix
+        elif goal == "ddg":
+            matrix_goal = SymNMF.ddg(vectors) # Calling ddg function in C to calculate the matrix
+        elif goal == "norm":
+            matrix_goal = SymNMF.norm(vectors) # Calling norm function in C to calculate the matrix  
+        elif goal == "symnmf":
+            matrix_goal = doSymnmf(vectors, k)
+        else:
+            print("An Error Has Occurred")
+            return
 
         # print matrix_goal until 4 decimal points
-        for centroid in matrix_goal:
-            print(','.join(format(x, ".4f") for x in centroid))
+        for row in matrix_goal:
+            print(','.join(format(x, ".4f") for x in row))
 
     except Exception:
         print("An Error Has Occurred")
@@ -60,6 +82,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# TODO:
-# - Implement API for sym, ddg, norm, symnmf
