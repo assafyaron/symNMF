8c75f9ae1d150f3855b6a950458ae7f8c48c4b55
assafyaron <128134935+assafyaron@users.noreply.github.com>
2024-10-28 21:09:07 +0200

    Update symnmf.h final_v

--- symnmf.h
+++ symnmf.h
@@ -2,9 +2,11 @@
 #ifndef SYMNMF_H
 #define SYMNMF_H
 
+void matrix_free(double **p, int n);
+double** matrix_malloc(double** new_matrix, int n, int m);
 double** sym(double** vectors, int N, int vecdim);
 double** ddg(double** vectors, int N, int vecdim);
 double** norm(double** vectors, int N, int vecdim);
-double** symnmf(double** W, double** H, int N, int vecdim, int k, int iter, double eps);
+double** symnmf(double** W, double** H, int N, int k);
 
 #endif
